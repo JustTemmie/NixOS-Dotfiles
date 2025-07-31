fc8f1ed89502f2339c12c6c332e7a76dedb699d6
beaver <git@beaver.mom>
2025-07-31 21:46:27 +0200

    rice kitty

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -7,6 +7,13 @@
     font = {
       name = "FiraCode Nerd Font";
       size = 13;
-    };    
+    };
+
+    settings = {
+      scrollback_indicator_opacity = 0.5;
+      background_opacity = 0.7;
+      hide_window_decorations = true;
+      window_padding_width = 6;
+    };
   };
 }
