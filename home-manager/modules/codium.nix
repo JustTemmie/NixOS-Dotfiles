67856f2afc8b2452698ec130231bfd56d9ef41f8
beaver <git@beaver.mom>
2025-08-01 14:16:44 +0200

    confgiure codium keybinds and addons

--- home-manager/modules/codium.nix
+++ home-manager/modules/codium.nix
@@ -6,6 +6,7 @@
     package = pkgs.vscodium.fhs;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       ms-python.python
+      magicstack.magicpython
       vue.volar
       ritwickdey.liveserver
       rust-lang.rust-analyzer
@@ -17,27 +18,42 @@
       pkief.material-icon-theme
     ];
 
-    profiles.default.userSettings = {
-      "files.autoSave" = "afterDelay";
-      "files.exclude" = {
-        "**/.git" = true;
-        "**/.svn" = true;
-        "**/.hg" = true;
-        "**/CVS" = true;
-        "**/.DS_Store" = true;
-        "**__pycache__/" = true;
+    profiles.default = {
+      keybindings = [
+        {
+          "key" = "ctrl+d";
+          "command" = "-editor.action.addSelectionToNextFindMatch";
+          "when" = "editorFocus";
+        }
+        {
+            "key" = "ctrl+d";
+            "command" = "editor.action.copyLinesDownAction";
+            "when" = "editorTextFocus && !editorReadonly";
+        }
+      ];
+
+      userSettings = {
+        "files.autoSave" = "afterDelay";
+        "files.exclude" = {
+          "**/.git" = true;
+          "**/.svn" = true;
+          "**/.hg" = true;
+          "**/CVS" = true;
+          "**/.DS_Store" = true;
+          "**__pycache__/" = true;
+        };
+        "editor.fontFamily" = "'Comic Code Ligatures', Regular";
+        "editor.fontLigatures" = true;
+        "git.enableCommitSigning" = true;
+        "git-graph.repository.sign.commits" = true;
+        "git-graph.repository.sign.tags" = true;
+        "git-graph.repository.commits.showSignatureStatus" = true;
+        "git.blame.editorDecoration.enabled" = true;
+        # "terminal.integrated.shellIntegration.enabled" = true;
+        # "terminal.integrated.stickyScroll.enabled" = true;
+        "workbench.colorTheme" = "Material Theme Darker High Contrast";
+        "workbench.iconTheme" = "material-icon-theme";
       };
-      "editor.fontFamily" = "'Comic Code Ligatures', Regular";
-      "editor.fontLigatures" = true;
-      "git.enableCommitSigning" = true;
-      "git-graph.repository.sign.commits" = true;
-      "git-graph.repository.sign.tags" = true;
-      "git-graph.repository.commits.showSignatureStatus" = true;
-      "git.blame.editorDecoration.enabled" = true;
-      # "terminal.integrated.shellIntegration.enabled" = true;
-      # "terminal.integrated.stickyScroll.enabled" = true;
-      "workbench.colorTheme" = "Material Theme Darker High Contrast";
-      "workbench.iconTheme" = "material-icon-theme";
     };
   };
 }
