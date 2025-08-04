9e8d6189702254e8b6dfd0c23d50a994bf1f2f3c
Temmie <git@beaver.mom>
2025-08-04 20:00:53 +0200

    change codium theme

--- home-manager/modules/codium.nix
+++ home-manager/modules/codium.nix
@@ -46,9 +46,7 @@
         "git-graph.repository.sign.tags" = true;
         "git-graph.repository.commits.showSignatureStatus" = true;
         "git.blame.editorDecoration.enabled" = true;
-        # "terminal.integrated.shellIntegration.enabled" = true;
-        # "terminal.integrated.stickyScroll.enabled" = true;
-        "workbench.colorTheme" = "Material Theme Darker High Contrast";
+        "workbench.colorTheme": "Maple Dark";
         "workbench.iconTheme" = "material-icon-theme";
       };
     };
