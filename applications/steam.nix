fc7d7fb519a833490118b2fb712ffce5a870047e
Temmie <git@beaver.mom>
2025-08-04 08:45:56 +0200

    try fixing gamescope

--- applications/steam.nix
+++ applications/steam.nix
@@ -1,4 +1,4 @@
-{
+{ config, pkgs, ... }: {
   programs.appimage.enable = true;
   programs.appimage.binfmt = true;
 
@@ -8,8 +8,28 @@
       capSysNice = true;
     };
     steam = {
+      package = pkgs.steam.override {
+        extraPkgs = pkgs': with pkgs'; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib # Provides libstdc++.so.6
+          libkrb5
+          keyutils
+          libei
+          gamescope
+        ];
+      };
+
       enable = true;
       gamescopeSession.enable = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
     };
   };
 }
