2f7ec9eaa272541667102ee8f421b8a58ec26b23
Temmie <git@beaver.mom>
2025-08-04 03:26:58 +0200

    set default git branch to "main"

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -2,11 +2,12 @@
   programs.git = {
     enable = true;
     diff-so-fancy.enable = true;
+    signing.signByDefault = true;
     userEmail = "git@beaver.mom";
     userName = "Temmie";
-    signing.signByDefault = true;
 
     extraConfig = {
+      init.defaultBranch = "main";
       commit.gpgsign = true;
       gpg.format = "ssh";
       user.signingkey = "/home/twig/.ssh/id_ed25519";
