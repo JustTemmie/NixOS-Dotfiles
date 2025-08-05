97a7f60dad36af9ca0b2e4ef4369870a4629d6f0
Temmie <git@beaver.mom>
2025-08-05 03:59:19 +0200

    hopefully fix git settings?!?!!!?!?!?

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -2,16 +2,21 @@
   programs.git = {
     enable = true;
     diff-so-fancy.enable = true;
-    signing.signByDefault = true;
     userEmail = "git@beaver.mom";
     userName = "Temmie";
+
+    signing = {
+      signByDefault = true;
+      format = "ssh";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNd7ErkwCiFOOTU/imXWO/fbwJS6rSq5XJjplEK02Nm";
+    };
 
     extraConfig = {
       push.autoSetupRemove = true;
       init.defaultBranch = "main";
       commit.gpgsign = true;
       gpg.format = "ssh";
-      user.signingkey = "/home/twig/.ssh/id_ed25519";
+      user.signingkey = "/home/twig/.ssh/id_ed25519.pub";
     };
   };
 }
