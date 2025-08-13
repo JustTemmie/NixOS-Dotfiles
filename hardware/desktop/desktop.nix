b9bef0596356639dd84f5436a72c6d74fbea1b42
Temmie <git@beaver.mom>
2025-08-13 13:01:12 +0200

    remove network config for built in ethernet

--- hardware/desktop/desktop.nix
+++ hardware/desktop/desktop.nix
@@ -30,12 +30,6 @@
         prefixLength = 24;
       }];
     };
-    interfaces.enp17s0 = {
-      ipv4.addresses = [{ 
-        address = "192.168.1.101";
-        prefixLength = 24;
-      }];
-    };
     defaultGateway = {
       address = "192.168.1.1";
       interface = "enp19s0";
