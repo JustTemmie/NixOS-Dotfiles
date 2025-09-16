a62b2778a0c816485ceb3061c84b129395f7f7ae
Twig <git@beaver.mom>
2025-09-16 14:57:48 +0200

    fix gamescope

--- applications/steam.nix
+++ applications/steam.nix
@@ -2,39 +2,22 @@
   programs.appimage.enable = true;
   programs.appimage.binfmt = true;
 
-  programs = {
-    gamescope = {
-      enable = true;
-      capSysNice = true;
-    };
-    steam = {
-      enable = true;
-      gamescopeSession.enable = true;
-
-      package = pkgs.steam.override {
-        extraLibraries = pkgs: [
-          pkgs.xorg.libxcb
-        ];
-        extraPkgs = pkgs': with pkgs'; [
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib # Provides libstdc++.so.6
-          libkrb5
-          keyutils
-          libei
-
-          gamescope
-          mangohud
-        ];
-      };
-
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
+  environment.systemPackages = with pkgs; [
+    gamescope
+  ];
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
       ];
     };
   };
