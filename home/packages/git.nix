6e86bbb8f96a128c856e6e62c41c0d63ccd6a34c
Twig <git@beaver.mom>
2025-10-20 15:05:11 +0200

    fix warnings

--- home/packages/git.nix
+++ home/packages/git.nix
@@ -1,9 +1,10 @@
 { config, pkgs, ... }: {
   programs.git = {
     enable = true;
-    diff-so-fancy.enable = true;
-    userEmail = "git@beaver.mom";
-    userName = "Twig";
+    user= {
+      email = "git@beaver.mom";
+      name = "Twig";
+    };
 
     signing = {
       signByDefault = true;
@@ -11,7 +12,7 @@
       key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNd7ErkwCiFOOTU/imXWO/fbwJS6rSq5XJjplEK02Nm";
     };
 
-    extraConfig = {
+    settings = {
       push.autoSetupRemove = true;
       branch.autoSetupMerge = true;
       init.defaultBranch = "main";
