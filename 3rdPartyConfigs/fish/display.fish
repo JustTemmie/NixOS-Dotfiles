5bae5c8038d83e9da173cefbbe2df49ea22f1d54
Twig <git@beaver.mom>
2025-10-22 04:27:42 +0200

    tab spacing

--- 3rdPartyConfigs/fish/display.fish
+++ 3rdPartyConfigs/fish/display.fish
@@ -13,30 +13,30 @@ function fish_prompt
 		set git_branch (set_color cyan)"($git_branch_status) "(set_color normal)
 	end
 
-        # show runtime if the command took more than 5 seconds to run
-        set -l time_str
-        if set -q CMD_DURATION
-            if test $CMD_DURATION -gt 5000
-                set -l total_seconds (math -s 0 "$CMD_DURATION / 1000")
-                if test $total_seconds -ge 3600
-                    set -l hours (math -s 0 "$total_seconds / 3600")
-                    set -l minutes (math -s 0 "($total_seconds % 3600) / 60")
-                    set -l seconds (math "$total_seconds % 60")
-                    set time_str "($hours:"(printf '%02d' $minutes)":"(printf '%02d' $seconds)") "
-                else if test $total_seconds -ge 60
-                    set -l minutes (math -s 0 "$total_seconds / 60")
-                    set -l seconds (math "$total_seconds % 60")
-                    set time_str "($minutes:"(printf '%02d' $seconds)") "
-                else
-                    set time_str "($total_seconds) "
-                end
+    # show runtime if the command took more than 5 seconds to run
+    set -l time_str
+    if set -q CMD_DURATION
+        if test $CMD_DURATION -gt 5000
+            set -l total_seconds (math -s 0 "$CMD_DURATION / 1000")
+            if test $total_seconds -ge 3600
+                set -l hours (math -s 0 "$total_seconds / 3600")
+                set -l minutes (math -s 0 "($total_seconds % 3600) / 60")
+                set -l seconds (math "$total_seconds % 60")
+                set time_str "($hours:"(printf '%02d' $minutes)":"(printf '%02d' $seconds)") "
+            else if test $total_seconds -ge 60
+                set -l minutes (math -s 0 "$total_seconds / 60")
+                set -l seconds (math "$total_seconds % 60")
+                set time_str "($minutes:"(printf '%02d' $seconds)") "
+            else
+                set time_str "($total_seconds) "
             end
         end
+    end
 
-        set -l status_entry "$stat"
-        set -l time_entry "$(set_color 72ad00)$time_str"
-        set -l state_entry "$(set_color f5a9b8)$(whoami)$(set_color white)@$(set_color 5bcefa)$(uname -n)"
-        set -l path_entry "$(set_color green)$(prompt_pwd --full-length-dirs 2)"
+    set -l status_entry "$stat"
+    set -l time_entry "$(set_color 72ad00)$time_str"
+    set -l state_entry "$(set_color f5a9b8)$(whoami)$(set_color white)@$(set_color 5bcefa)$(uname -n)"
+    set -l path_entry "$(set_color green)$(prompt_pwd --full-length-dirs 2)"
 
 	string join '' -- $git_branch $stat $time_entry $state_entry ' ' $path_entry (set_color normal) ' '
 end
