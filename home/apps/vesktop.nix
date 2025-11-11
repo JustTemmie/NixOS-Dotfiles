e2d94b2a44a712ea60579e371452dd4f75719d73
Twig <git@beaver.mom>
2025-11-11 21:21:53 +0100

    implement xdg mime tyteps

--- home/apps/vesktop.nix
+++ home/apps/vesktop.nix
@@ -1,16 +1,10 @@
-{
+{ pkgs, ... }: {
   programs.vesktop.enable = true;
 
-  home-manager.users.myuser = {
-    xdg.mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "text/html" = "librewolf.desktop";
-        "x-scheme-handler/http" = "librewolf.desktop";
-        "x-scheme-handler/https" = "librewolf.desktop";
-        "x-scheme-handler/about" = "librewolf.desktop";
-        "x-scheme-handler/unknown" = "librewolf.desktop";
-      };
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/discord" = "vesktop.desktop";
     };
   };
 }
