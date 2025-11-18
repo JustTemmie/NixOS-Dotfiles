997bca6a7b8ea3b58b7138127948d772b4d1f27f
Twig <git@beaver.mom>
2025-11-18 15:37:18 +0100

    merge

--- home/apps/vesktop.nix
+++ home/apps/vesktop.nix
@@ -4,15 +4,7 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-<<<<<<< HEAD
       "x-scheme-handler/discord" = "vesktop.desktop";
-=======
-      "text/html" = "librewolf.desktop";
-      "x-scheme-handler/http" = "librewolf.desktop";
-      "x-scheme-handler/https" = "librewolf.desktop";
-      "x-scheme-handler/about" = "librewolf.desktop";
-      "x-scheme-handler/unknown" = "librewolf.desktop";
->>>>>>> origin/main
     };
   };
 }
