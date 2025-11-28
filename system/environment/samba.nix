19b5ce5afb0fd73f831480fd6d359651cb57c6e7
Twig <git@beaver.mom>
2025-11-28 20:22:29 +0100

    revert f3fc4632f5a3c58d108fde7d4e9a3434d831d80a
    revert update samba mount options

--- system/environment/samba.nix
+++ system/environment/samba.nix
@@ -1,17 +1,15 @@
 { config, lib, pkgs, ... }:
   let
     options = [
-      "noperm"
       "x-systemd.automount"
+      "noauto"
       "x-systemd.idle-timeout=60"
       "x-systemd.device-timeout=5s"
       "x-systemd.mount-timeout=5s"
-      "x-systemd.requires=network-online.target"
-      "rw"
+      "user"
+      "users"
       "credentials=/etc/nixos/private/smb-secrets"
       "uid=1000"
-      "gid=100"
-      "_netdev"
     ];
   in
 {
