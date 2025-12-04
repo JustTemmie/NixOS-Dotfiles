c8b773fad81283c1669e06988dc9789c68ec3e42
Twig <git@beaver.mom>
2025-12-04 04:45:10 +0100

    remove unused mae imports

--- home/mae.nix
+++ home/mae.nix
@@ -3,17 +3,13 @@
     ./apps/codium.nix
     ./apps/firefox.nix
     ./apps/fish.nix
-    # ./apps/git.nix
     ./apps/kitty.nix
     ./apps/ncmpcpp.nix
     ./apps/prismlauncher.nix
     ./apps/python.nix
     ./apps/rmpc.nix
-    # ./apps/ssh.nix
     ./apps/vesktop.nix
 
-    # ./services/mpd-discord-rpc.nix steam presence
-    # ./services/mpd-scribble.nix
     ./services/mpd.nix
 
     ./user-packages.nix
